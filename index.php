d74b977cad1584fa83d20b12a98d5cdb9d963b69
atish anand nayak <an478@afsconnect2.njit.edu>
2017-10-19 23:48:36 -0400

    Project completion

--- index.php
+++ index.php
@@ -74,30 +74,95 @@ abstract class page
  }
 } 
 
+class getstatic
+{
+  static public function upform()
+  {
+    //construct the upload form using HTML tags
+        $form = '<form action="index.php?page=upload" method="post" enctype="multipart/form-data">';
+        $form .= '<input type="file" name="fileToUpload" id="filetoupload"><br><br>';
+        $form .= '<input type="submit" value="Upload File" name="submit">';
+        $form .= '</form>';
+        return $form;
+  }
+}
+
+class headerstatic
+{
+  static public function sendto($target_file)
+  {
+    //direct the user to the table class by passing the parameters to the URL
+   header('Location: index.php?page=table&doc=' . $target_file);
+  }
+}
 
 class upload extends page
 {
 	public function get()
 	{   
-		//construct the upload form using HTML tags
-        $form = '<form action="index.php?page=upload" method="post" enctype="multipart/form-data">';
-        $form .= '<input type="file" name="fileToUpload" id="filetoupload"><br><br>';
-        $form .= '<input type="submit" value="Upload File" name="submit">';
-        $form .= '</form>';
-        $this->html .= '<h1><i>Upload Form</i></h1><hr>';
-        $this->html .= $form;
+    $this->html .= '<h1><i>Upload Form</i></h1><hr>';
+    //call the static function 
+    $this->html .= getstatic::upform();
 	}
 
 	public function post()
 	{   
 		//set the path where the files will be saved
-        
+    $target_dir = "/afs/cad/u/a/n/an478/public_html/WSD_project1/uploads/";
+
+    //store the path to the file where it is saved
+    $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+
+        //Check the file size
+        if ($_FILES["fileToUpload"]["size"] > 500000)
+        {
+          echo "Sorry, your file is too large.";       
+        }
+
+      
+           //move the files to the specified location from, the browser
+       if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) 
+       {
+         //call static functio that uses header function
+         headerstatic::sendto($target_file);
+       }  
+       else 
+       {
+         echo "Sorry, there was an error uploading your file.";
+      
+       }        
     }
 }
 
 class table extends page
 {
   
+  public function get()
+  {  
+   //open the file taken from the header function 
+   $filedoc = $_GET['doc'];
+   //open the file
+   $handle = fopen($filedoc,"r") or die('Cannot open file:  '.$filedoc);
+   
+   //create table 
+    $display = '<table border="1">';
+    while(!feof($handle))
+    {
+      $ay = fgetcsv($handle);//reads from the file while seperating values
+      $arry = (array) $ay;
+      $display .= '<tr>';
+      foreach($arry as $value) 
+      {
+        $display .= '<th>'.$value.'</th>';
+      }
+      $display .= '</tr>'; 
+    }
+    $display .= '</table>';   
+    $this->html .= $display;
+
+    //close the opned file
+   fclose($handle);
+  }
 }
 ?>
  
